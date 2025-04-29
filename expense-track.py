ce96df1991b87a5f09144f91d83513a40b66a397
apigituser <responsemail850@gmail.com>
2025-04-29 20:52:30 +0530

    it gets worse before it gets better

--- expense-track.py
+++ expense-track.py
@@ -1,6 +1,5 @@
 import csv
 import os
-import sys
 import argparse
 import datetime
 
@@ -14,21 +13,30 @@ def add_expense():
     if(os.stat("expenses.csv").st_size == 0):
         with open("expenses.csv", "w", newline='') as csv_file:
             writer = csv.writer(csv_file)
-            for row in rows:
-                writer.writerow(row)
+            writer.writerow([1, date, args.description, args.amount])
+            print(f"Expense added successfully (ID:1)")
     else:
         with open("expenses.csv", "a", newline='') as csv_file:
+            id = entries + 1
             writer = csv.writer(csv_file)
+            writer.writerow([id, date, args.description, args.amount])
+            print(f"Expense added successfully (ID:{id})")
             
 def delete_expense():
-    return
+    id = args.id
+    print(f"Expense deleted (ID:{id})")
 
 def summarise():
-    return
+    with open("expenses.csv") as csv_file:
+        reader = csv.reader(csv_file)
+        print(f"Total expenses: ${sum([int(row[3]) for row in reader])}")
 
 if __name__ == "__main__":
-    timestamp = datetime.datetime.now().strftime("%Y-%m-%d")
+    date = datetime.datetime.now().strftime("%Y-%m-%d")
+
+
     parser = argparse.ArgumentParser()
+    parser.add_argument('action')
     parser.add_argument('--id')
     parser.add_argument('--month')
     parser.add_argument('--description')
@@ -36,18 +44,22 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     file_exists = os.path.isfile("expenses.csv")
-
-    action = sys.argv[1]
     if file_exists:
-        match action:
-            case "add":
-                add_expense()
-            case "list":
+        with open("expenses.csv") as csv_file:
+            reader = csv.reader(csv_file)
+            entries = sum(1 for row in reader)
+
+        match args.action:
+            case 'list':
                 list_expenses()
-            case "delete":
-                delete_expense()
-            case "summary":
+            case 'add':
+                add_expense()
+            case 'summary':
                 summarise()
+            case 'delete':
+                delete_expense()
+            case _:
+                print("Invalid Argument")
     else: 
         print("File not found")
         exit(1)
